42a00e46cd32ca4c71fd1ea4e189775e3c563eb4
moonjiye <mjy6088@gmail.com>
2024-06-03 21:56:04 +0900

    24/06/03 뭉 호텔CRUD

--- src/main/java/kr/happytravel/erp/sales/service/HotelService.java
+++ src/main/java/kr/happytravel/erp/sales/service/HotelService.java
@@ -1,5 +1,6 @@
 package kr.happytravel.erp.sales.service;
 
+import kr.happytravel.erp.sales.model.sales.HotelDTO;
 import kr.happytravel.erp.sales.model.sales.HotelModel;
 import java.util.List;
 import java.util.Map;
@@ -11,11 +12,13 @@ public interface HotelService {
     // 호텔 수 조회
     int getHotelCnt(Map<String, Object> paramMap) throws Exception;
 
+    String getLastHotelCode() throws Exception;
+
     // 호텔 단건 조회
-    HotelModel selectHotel( Map<String, Object> paramMap ) throws Exception;
+    HotelModel selectHotel(HotelDTO hotel) throws Exception;
 
     // 호텔 단건 등록
-    int insertHotel(Map<String, Object> paramMap) throws Exception;
+    int insertHotel(HotelDTO hotel) throws Exception;
 
     // 호텔 단건 수정
     int updateHotel(Map<String, Object> paramMap) throws Exception;
